67132e632430e799042f86a5da5508a008f3a50a
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-08-14 15:26:53 -0700

    build out flavorController w/out authentication

---
+++ SweetAndSavory/Controllers/FlavorController.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using SweetAndSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SweetAndSavory.Controllers
+{
+  public class BooksController : Controller
+  {
+    private readonly SweetAndSavoryContext _db;
+
+    public BooksController(SweetAndSavoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+    public ActionResult Create()
+    {
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor, int TreatId)
+    {
+      _db.Flavors.Add(flavor);
+      if (TreatId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      //add authentication
+      var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.Treats)
+          .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor, int TreatId)
+    {
+      if (TreatId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavor.FlavorId });
+    }
+    public ActionResult Delete(int id)
+    {
+      var ThisFlavor = _db.Flavors.FirstOrDefault(a => a.FlavorId == id);
+      return View(ThisFlavor);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var ThisFlavor = _db.Flavors.FirstOrDefault(a => a.FlavorId == id);
+      _db.Flavors.Remove(ThisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
